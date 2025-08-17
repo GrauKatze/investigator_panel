229770d8dcc01f8ad86d6cfd13a759a34ec9b66d
GrauKatze <GrauKatze@protonmail.com>
2025-08-17 20:37:48 +0300

    add base function

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,3 +1,56 @@
-fn main(){
-    println!("CLI");
+pub enum ConfigType {
+    /// Вывод справки
+    Help,
+    /// Вывод версии приложения
+    Version,
+}
+
+pub struct Config {
+    pub path: String,
+    pub input: Option<String>,
+    pub _output: Option<String>,
+    pub args: Vec<String>,
+}
+
+fn main() {
+    init(std::env::args().skip(1).collect());
+}
+
+fn write_help() {
+    println!(env!("CARGO_PKG_NAME"));
+    println!("version: {}", env!("CARGO_PKG_VERSION"));
+    println!(env!("CARGO_PKG_DESCRIPTION"));
+    println!("\nUsage: {} [key]\n", env!("CARGO_PKG_NAME"));
+    println!("KEYS:");
+    println!("{:25} {}", "-h | --help", "this text");
+    println!("{:25} {}", "-v | --version", "version");
+}
+pub fn args_pars(args: Vec<String>) -> Result<ConfigType, String> {
+    if args.len() > 0 {
+        match args[0].as_str() {
+            "-h" | "--help" => Ok(ConfigType::Help),
+            "-v" | "--version" => Ok(ConfigType::Version),
+            _ => Err(format!("not found this argument\n{:?}", args)),
+        }
+    } else {
+        Err("need more argument\n".to_string())
+    }
+}
+pub fn init(args: Vec<String>) {
+    match args_pars(args) {
+        Ok(config_type) => match config_type {
+            ConfigType::Help => {
+                write_help();
+                std::process::exit(0);
+            }
+            ConfigType::Version => {
+                println!("version: {}", env!("CARGO_PKG_VERSION"));
+                std::process::exit(0);
+            }
+        },
+        Err(msg) => {
+            println!("{}", msg);
+            std::process::exit(1);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,47 @@
 #![warn(missing_docs)]
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! # Panel
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+///module for analyst work
+mod analyst_panel {
+    fn _file_ident(file_path: String) {
+        analyst::file_identification::_file_ident(file_path).unwrap();
     }
 }
 
+///module for extractor work
+mod extractor_panel {
+    pub fn _file_extract(src: String, dst: String) -> Result<(), String> {
+        if _is_path_valid(&src) && _is_path_valid(&dst) {
+            Ok(())
+        } else {
+            Err("err".to_string())
+        }
+    }
+    pub fn _extract_from_file(src: String, dst: String) -> Result<(), String> {
+        match extractor::check_file(&src) {
+            Ok(()) => {
+                if _is_path_valid(&src) && _is_path_valid(&dst) {
+                    Ok(())
+                } else {
+                    Err("err".to_string())
+                }
+            }
+            Err(msg) => Err(msg),
+        }
+    }
+    fn _is_path_valid(_path: &String) -> bool {
+        true
+    }
+}
+
+///module for register work
+mod register_panel {
+    fn _reg_file(file_path: String){
+        register::take_file(file_path).unwrap()
+    }
+
+    fn _take_file(file_path: String){
+        register::give_file(file_path).unwrap()
+    }
+}
