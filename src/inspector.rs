8908f53f6f5e6aace11380d47f8c5aaef8856231
GrauKatze <GrauKatze@protonmail.com>
2026-03-03 15:36:10 +0300

    add interfase

--- src/inspector.rs
+++ src/inspector.rs
@@ -1,3 +1,15 @@
+use crate::Manual;
+
+struct Inspector;
+
+impl Manual for Inspector {
+    fn run(_opt: Option<Vec<String>>)->Result<(),String> {
+        Ok(())
+    }
+}
+
+
+
 pub fn run(_src: Option<Vec<String>>) -> Result<(), String> {
     println!("INSPECTOR");
     Ok(())
